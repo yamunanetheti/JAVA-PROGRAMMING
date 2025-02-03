d5e005fbe9efac6af1914fb5e4bf5350cae5c23a
yamunanetheti <yamunanetheti01@gmail.com>
2025-02-03 16:24:33 +0530

    Add files via upload

---
+++ VariablesAndDataTypes.java
@@ -0,0 +1,17 @@
+public class VariablesAndDataTypes {
+    public static void main(String[] args) {
+        // Declare and initialize different data types
+        int integerVar = 10;
+        double doubleVar = 20.5;
+        char charVar = 'A';
+        String stringVar = "Hello, This is Aswin";
+        boolean boolVar = true;
+
+        // Display the values of the variables
+        System.out.println("Integer: " + integerVar);
+        System.out.println("Double: " + doubleVar);
+        System.out.println("Character: " + charVar);
+        System.out.println("String: " + stringVar);
+        System.out.println("Boolean: " + boolVar);
+    }
+}
